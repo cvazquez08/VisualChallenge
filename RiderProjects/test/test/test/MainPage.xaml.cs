8e5da5966df7c0cce2e71457befa8a83f4c570cf
Chris Vazquez <chris.vazquez@mm-vazquezc.peace.daveramsey.com>
2021-01-12 13:25:59 -0600

    done

---
+++ RiderProjects/test/test/test/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace test
+{
+    
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public List<Color> Podcasts { get; set; }
+        public MainPage()
+        {
+            BindingContext = this;
+
+            Podcasts = new List<Color>
+            {
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+
+            };
+            InitializeComponent();
+        }
+    }
+}
